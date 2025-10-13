e3ce0d0df11805ac59a87c5246d3dc35275a9f29
tanset <haoyuchang8@gmail.com>
2025-10-13 10:25:18 -0400

    create main.go and create TaxIncludedPriceJob struct

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tanset/Price-Calculator
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/tanset/Price-Calculator/prices"
+)
+func main(){
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+
+	for _, taxRate := range taxRates{
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
+	}
+}
